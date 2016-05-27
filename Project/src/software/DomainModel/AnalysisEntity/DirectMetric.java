d55c00f2fd48344c4df0a158c246dd25227f5735
eva <villarrealguzman@gmail.com>
2016-05-27 20:10:00 -0300

    Agregado de Mica

--- Project/src/software/DomainModel/AnalysisEntity/DirectMetric.java
+++ Project/src/software/DomainModel/AnalysisEntity/DirectMetric.java
@@ -1,17 +1,19 @@
 package software.DomainModel.AnalysisEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 /**
  * This class defines metrics
- * 
+ *
  * @author: FEM
  * @version: 04/05/2016
  */
 
 @Entity
-@Table(name = "DIRECTMETRICS")
+@Table(name = "DIRECTMETRICS" )
+@PrimaryKeyJoinColumn(name="id" )
 public class DirectMetric extends Metric {
 
 }
--- Project/src/software/DomainModel/AnalysisEntity/IndirectMetric.java
+++ Project/src/software/DomainModel/AnalysisEntity/IndirectMetric.java
@@ -6,40 +6,45 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 /**
  * This class defines metrics
- * 
+ *
  * @author: FEM
  * @version: 04/05/2016
  */
 
 @Entity
-@Table(name = "INDIRECTMETRICS")
+@Table(name = "INDIRECTMETRICS" )
+@PrimaryKeyJoinColumn(name="id" )
 public class IndirectMetric extends Metric {
 
-	@OneToMany(targetEntity = Metric.class, cascade = CascadeType.ALL)
-	private Set<Metric> metrics = new HashSet<Metric>();
+        @OneToMany(targetEntity = Metric.class, cascade = CascadeType.ALL)
+        private Set<Metric> metrics = new HashSet<Metric>();
 
-	// Composite pattern
+        // Composite pattern
 
-	public void add(Metric pmetric) {
-		metrics.add(pmetric);
-	}
+        public void add(Metric pmetric) {
+               metrics.add( pmetric);
+       }
 
-	public void delete(Metric pmetric) {
-		metrics.remove(pmetric);
-	}
+        public void delete(Metric pmetric) {
+               metrics.remove( pmetric);
+       }
 
-	// Getters and Setters
+        // Getters and Setters
 
-	public Set<Metric> getMetrics() {
-		return metrics;
-	}
+        public Set<Metric> getMetrics() {
+               return metrics ;
+       }
 
-	public void setMetrics(Set<Metric> metrics) {
-		this.metrics = metrics;
-	}
-
+        public void setMetrics(Set<Metric> metrics) {
+               this.metrics = metrics ;
+       }
 }
+
+
+
+
