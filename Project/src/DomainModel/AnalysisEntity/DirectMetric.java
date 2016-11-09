6d4bee8d75e63696d68c6cdbc9c2f3b1ee24027e
Eva <villarrealguzman@gmail.com>
2016-11-09 14:02:38 -0300

    Clases que faltaban arriba

---
+++ Project/src/DomainModel/AnalysisEntity/DirectMetric.java
@@ -0,0 +1,24 @@
+package DomainModel.AnalysisEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+/**
+ * This class defines metrics
+ *
+ * @author: FEM
+ * @version: 04/05/2016
+ */
+
+/**
+ * borrar
+ * @author Usuario-Pc
+ *
+ */
+@Entity
+@Table(name = "DIRECTMETRICS" )
+@PrimaryKeyJoinColumn(name="id" )
+public class DirectMetric extends Metric {
+
+}
---
+++ Project/src/DomainModel/AnalysisEntity/IndirectMetric.java
@@ -0,0 +1,51 @@
+package DomainModel.AnalysisEntity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+/**
+ * This class defines metrics
+ *
+ * @author: FEM
+ * @version: 04/05/2016
+ */
+
+/**
+ * borrar
+ * @author Usuario-Pc
+ *
+ */
+@Entity
+@Table(name = "INDIRECTMETRICS")
+@PrimaryKeyJoinColumn(name = "id")
+public class IndirectMetric extends Metric {
+
+	@OneToMany(targetEntity = Metric.class, cascade = CascadeType.ALL)
+	private Set<Metric> metrics = new HashSet<Metric>();
+
+	// Composite pattern
+
+	public void add(Metric pmetric) {
+		metrics.add(pmetric);
+	}
+
+	public void delete(Metric pmetric) {
+		metrics.remove(pmetric);
+	}
+
+	// Getters and Setters
+
+	public Set<Metric> getMetrics() {
+		return metrics;
+	}
+
+	public void setMetrics(Set<Metric> metrics) {
+		this.metrics = metrics;
+	}
+}
